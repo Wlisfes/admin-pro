9eaa84a150bc2adad654b28ebc91a7ca2c3112b5
情雨随风 <876451336@qq.com>
2020-07-01 11:10:20 +0800

    新增全屏组件

--- src/components/layout/Dropout.tsx
+++ src/components/layout/Dropout.tsx
@@ -6,10 +6,12 @@
  * @Description: 头部user组件
  */
 
+import './less/dropout.less'
 import { Component, Vue } from 'vue-property-decorator'
 import { namespace } from 'vuex-class'
 import { AppUser } from '@/store/modules/app/types'
 import { Dropdown, Avatar, Menu, Divider, Icon } from 'ant-design-vue'
+import screenfull from 'screenfull'
 
 const AppModule = namespace('app')
 
@@ -18,7 +20,40 @@ export default class Dropout extends Vue {
 	@AppModule.State(state => state.user) user!: AppUser | null
 	@AppModule.Action('logout') logout!: Function
 
-	async onMenuClick(params: { key: string }) {
+	private isFullscreen: boolean = false
+
+	protected mounted() {
+		if (screenfull.isEnabled) {
+			screenfull.on('change', this.fullScreenChange)
+		}
+	}
+
+	protected beforeDestroy() {
+		if (screenfull.isEnabled) {
+			screenfull.off('change', this.fullScreenChange)
+		}
+	}
+
+	//全屏切换事件
+	public fullScreen() {
+		if (screenfull.isEnabled) {
+			if (!screenfull.isFullscreen) {
+				screenfull.request()
+			} else {
+				screenfull.toggle()
+			}
+		}
+	}
+
+	//监听全屏切换事件
+	public fullScreenChange() {
+		if (screenfull.isEnabled) {
+			this.isFullscreen = screenfull.isFullscreen
+		}
+	}
+
+	//头像下拉事件
+	public async onMenuClick(params: { key: string }) {
 		switch (params.key) {
 			case 'logout':
 				await this.logout()
@@ -31,7 +66,12 @@ export default class Dropout extends Vue {
 
 	render() {
 		return (
-			<div class="user-drop">
+			<div class="root-user-drop">
+				<Icon
+					type={this.isFullscreen ? 'fullscreen-exit' : 'fullscreen'}
+					class="full"
+					onClick={this.fullScreen}
+				/>
 				<Dropdown>
 					<div style={{ cursor: 'pointer', padding: '0 12px' }}>
 						<Avatar src={`${this.user?.avatar}?x-oss-process=style/resize`} />
